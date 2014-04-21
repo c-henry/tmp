f446d1bd80e37463f465f5e112d9fb336eab0d8f
Henry <email@c-henry.com>
2014-04-21 17:06:49 -0400

    ruby practice. generating random string.

---
+++ learning_ruby/rand_gen.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+#puts "hello world"
+
+#(0...8).map { (65 + rand(26)).chr }.join
+puts "rand(26)"
+puts rand(26)
+puts "(65 + rand(26)).chr"
+puts (65 + rand(26)).chr
+#puts "{ (65 + rand(26)).chr }.join"
+#puts { (65 + rand(26)).chr }.join
+puts "(0...8).map { (65 + rand(26)).chr }.join"
+puts (0...8).map { (65 + rand(26)).chr }.join
+
+
+#require 'securerandom'
+#$i = 0
+#$num = 100
+#while $i < $num do 
+#   puts SecureRandom.base64(n=1)
+#   $i +=1
+#end
+
+
